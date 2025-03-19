dc385accc5757e8aa57672ae314dd63de0bc0fb1
Kauã Werle <kauawerle19@gmail.com>
2025-03-19 11:53:48 -0300

    Implement Validations

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfff55725e2bb20037c876c3c83e94dd4260dea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4a333ba0faa32e7f7a35a96b9c9ca1bbdd0b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
